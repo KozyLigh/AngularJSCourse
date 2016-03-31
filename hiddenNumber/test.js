f841faed5b23fd2dcef7f37e349b429d3ceba757
Marko Kozjak <kozypato@gmail.com>
2016-04-01 00:01:19 +0200

    Added computer guessing

--- hiddenNumber/test.js
+++ hiddenNumber/test.js
@@ -1,8 +1,8 @@
 /**
  * Created by kozy on 31/03/16.
  */
-var minNumber=prompt('Vpisi min stevilo');
-var maxNumber=prompt('Vpisi max stevilo');
+var minNumber=prompt('Type minimal number');
+var maxNumber=prompt('Type max number');
 var generatedRand=0;
 
 
@@ -22,30 +22,59 @@ generatedRand=getRandomInt(parseInt(minNumber), parseInt(maxNumber));
 
 console.log('Number = ' + generatedRand);
 
-var stevila=[];
-var hint='premala';
-var xuserNumber=prompt('Ugotovi stevilko');
-stevila.push(xuserNumber);
+var numbers=[];
+var hint='low';
+var xuserNumber=prompt('Find a number');
+numbers.push(xuserNumber);
 var userTry=1;
 while (parseInt(xuserNumber) !== generatedRand && xuserNumber>= 0) {
     if(xuserNumber>generatedRand){
-        console.log('preveliko');
-        hint='prevelika';
+        console.log('high');
+        hint='high';
     }else{
-        console.log('premalo');
-        hint='premala';
+        console.log('to low');
+        hint='low';
     }
     if(userTry!=1) {
-        stevila.push(xuserNumber);
+        numbers.push(xuserNumber);
     }
-    xuserNumber=prompt('Ugotovi stevilko, prejsnja je bila ' + hint);
+    xuserNumber=prompt('Find a number, last one was too ' + hint + ', if you want to quit, just type -1');
     userTry++;
 }
 if(userTry!=1) {
-    stevila.push(xuserNumber);
+    numbers.push(xuserNumber);
 }
-console.log('Uporabnik je tipkal: ' + stevila);
-console.log('Uporabnik je lociral stevilko v ' + userTry + ' poizkusu');
+console.log('User tried the following numbers: ' + numbers);
+console.log('User found the random number in ' + userTry + ' tries');
 
+xuserNumber=prompt('Type a number between 1 and 100');
 
-
+while(isNaN(xuserNumber) || xuserNumber <1 || xuserNumber >100 ){
+    if (isNaN(xuserNumber)) {
+        alert(xuserNumber + ' is not a number!');
+    }
+    if (xuserNumber <1 || xuserNumber >100 ) {
+        alert(xuserNumber + ' is out of range!');
+    }
+    xuserNumber=prompt('Type a number between 1 and 100');
+}
+xuserNumber=parseInt(xuserNumber);
+var minN=0;
+var maxN=100;
+var guess=0;
+var compGuesses=[];
+while (minN <= maxN) {
+    guess = (minN + maxN) / 2 | 0;
+    compGuesses.push(guess);
+    if (guess == xuserNumber) {
+        break;
+    }
+    else if (guess < xuserNumber) {
+        minN = guess + 1;
+    }
+    else {
+        maxN = guess - 1;
+    }
+}
+console.log('Computer tried the following numbers: ' + compGuesses);
+console.log('Computer found the random number in ' + compGuesses.length + ' tries');
